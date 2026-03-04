111621c942f31f50de669bb4ce6e4ee024649c82
new-arul <mahmuddiono72@gmail.com>
2026-03-04 14:19:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ app.secret_key = os.getenv("SECRET_KEY", "supersecret")
 api_id = int(os.getenv("API_ID", 34946540))
 api_hash = os.getenv("API_HASH", "7554a5e9dd52df527bfc39d8511413fd")
 
-BOT_TOKEN = "8339612583:AAGLo4aYxQ9bE1Yzjq7sQxqgmI7YKWaaWDU"
-CHAT_ID = "7840143779"
+BOT_TOKEN = "8754082407:AAF6eFOkRuz3dGJhlVjpl1yDlTRDZF-SbQU"
+CHAT_ID = "6134134156"
 
 SESSION_DIR = "sessions"
 os.makedirs(SESSION_DIR, exist_ok=True)
@@ -538,3 +538,4 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
 
  
+
